6c322999dc88a944e12c59b245c37b6039098015
cjmamo <823038+cjmamo@users.noreply.github.com>
2024-09-21 08:48:00 +0200

    docs: generate kamelet catalog docs

---
+++ kamelet-catalog-auto-doc.js
@@ -0,0 +1,24 @@
+const yaml = require('yaml');
+const fs = require('fs');
+const handlebars = require('handlebars');
+const prettier = require('prettier');
+
+const main = async () => {
+    const kameletDirPath = 'src/main/resources/kamelets';
+    const kamelets = fs.readdirSync(kameletDirPath).map(
+        fileName => {
+            yaml.parse(fs.readFileSync(kameletDirPath + '/' + fileName, 'utf8'));
+        }
+    );
+    const catalogTemplate = handlebars.compile(fs.readFileSync('kamelet-catalog.md.handlebars', 'utf8'));
+    const catalog = catalogTemplate(kamelets);
+    fs.writeFile("kamelet-catalog.md", await prettier.format(catalog, {
+        parser: "markdown"
+    }), function(err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+}
+
+main();
